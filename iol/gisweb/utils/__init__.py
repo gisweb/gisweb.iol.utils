a974b0fad3bace718131e734195303b828759403
vitolab <vito.labbe@hotmail.it>
2015-03-06 17:32:07 +0100

    Added class dateEncoder form DateTime encoding in json.dumps

--- iol/gisweb/utils/__init__.py
+++ iol/gisweb/utils/__init__.py
@@ -3,3 +3,12 @@ from AccessControl import allow_module
 allow_module('iol.gisweb.utils.interfaces')
 allow_module('iol.gisweb.utils.IolDocument')
 MessageFactory = MessageFactory('iol.gisweb.utils')
+
+from DateTime import DateTime
+import simplejson as json
+
+class dateEncoder(json.JSONEncoder):
+    def default(self,obj):
+        if isinstance(obj,DateTime):
+            return obj.strftime("%d/%m/%Y")
+        return json.JSONEncoder.default(self,obj)
