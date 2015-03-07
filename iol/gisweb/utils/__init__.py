868d37038523a35f74e7968304424fddd4dc62eb
mamogmx <marco.carbone@gmx.com>
2015-03-07 05:46:02 +0100

    added class loadJsonFile

--- iol/gisweb/utils/__init__.py
+++ iol/gisweb/utils/__init__.py
@@ -1,14 +1,26 @@
 from zope.i18nmessageid import MessageFactory
 from AccessControl import allow_module
+from DateTime import DateTime
+import simplejson as json
+import os
+
 allow_module('iol.gisweb.utils.interfaces')
 allow_module('iol.gisweb.utils.IolDocument')
 MessageFactory = MessageFactory('iol.gisweb.utils')
-
-from DateTime import DateTime
-import simplejson as json
 
 class dateEncoder(json.JSONEncoder):
     def default(self,obj):
         if isinstance(obj,DateTime):
             return obj.strftime("%d/%m/%Y")
         return json.JSONEncoder.default(self,obj)
+
+class loadJsonFile(object):
+    def __call__(self, file):
+        if not os.path.isfile(file):
+            return dict()
+        with open(file) as json_data:
+            try:
+                d = json.loads(json_data)
+            except:
+                return dict()
+            return d
