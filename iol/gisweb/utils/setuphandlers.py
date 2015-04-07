b59044706de8b10f37826d3818e8cbf32d78de6b
Marco Carbone <marco.carbone@gmx.com>
2015-04-07 09:27:34 +0200

    Update setuphandlers.py

--- iol/gisweb/utils/setuphandlers.py
+++ iol/gisweb/utils/setuphandlers.py
@@ -8,11 +8,14 @@ PROFILE_ID = 'iol.utils.replication:default'
 logger = logging.getLogger('iol.gisweb.utils')
 
 def initPackage(context):
-    catalog = api.portal.get_tool('portal_catalog')
-    brains = catalog(portal_type='PlominoDatabase')
-    for brain in brains:
-        db = brain.getObject()
-        for doc in db.getAllDocuments():
-            if not IIolDocument.providedBy(doc):
-                mark(doc,IIolDocument)
+    try:
+        catalog = api.portal.get_tool('portal_catalog')
+        brains = catalog(portal_type='PlominoDatabase')
+        for brain in brains:
+            db = brain.getObject()
+            for doc in db.getAllDocuments():
+                if not IIolDocument.providedBy(doc):
+                    mark(doc,IIolDocument)
+    except:
+        pass
 
